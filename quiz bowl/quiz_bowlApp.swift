95771bf5922015fe791117adfe11d6301c165898
Erin Chon <echon@bsd220.org>
2023-01-09 09:38:12 -0600

    Initial Commit

---
+++ quiz bowl/quiz_bowlApp.swift
@@ -0,0 +1,17 @@
+//
+//  quiz_bowlApp.swift
+//  quiz bowl
+//
+//  Created by Erin Chon on 1/9/23.
+//
+
+import SwiftUI
+
+@main
+struct quiz_bowlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
